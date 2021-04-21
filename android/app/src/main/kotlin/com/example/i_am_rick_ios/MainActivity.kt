08b908de4fed16565c2d8d98edf79bef38233fc3
Jason Jackson <jason@positioned.co>
2021-04-21 12:00:46 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/i_am_rick_ios/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.i_am_rick_ios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/candlestick.dart
@@ -0,0 +1,17 @@
+import 'package:app/candlestick_core.dart';
+
+class Candlestick {
+  final double open;
+  final double close;
+  final double high;
+  final double low;
+  final double volume;
+
+  Candlestick({
+    this.open,
+    this.close,
+    this.high,
+    this.low,
+    this.volume,
+  });
+}
